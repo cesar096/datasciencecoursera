07aed04dc2cc5c948755b749e94a2afd79238596
cesar096 <rodriguez.csr@gmail.com>
2020-09-01 12:19:16 -0430

    project tidydata

---
+++ Code-book.md
@@ -0,0 +1,27 @@
+R Markdown
+----------
+
+1.  Download the data
+
+2.  Assing variables &lt;- data
+
+features &lt;- features.txt activities &lt;- activity\_labels.txt
+subject\_test &lt;- test/subject\_test.txt x\_test &lt;-
+test/X\_test.txt y\_test &lt;- test/y\_test.txt subject\_train &lt;-
+test/subject\_train.txt x\_train &lt;- test/X\_train.txt y\_train &lt;-
+test/y\_train.txt
+
+1.  Merges training and test data
+
+Merged\_Data
+
+1.  Extract mean and standard deviation
+
+TidyData
+
+1.  Correct names for variables
+
+Acc by Accelerometer Gyro by Gyroscope BodyBody by Body Mag by Magnitude
+f by Frequency t by Time
+
+1.  Create a final data FinalData
---
+++ run_analysis.R
@@ -0,0 +1,53 @@
+Library (dplyr
+         )
+#Validar que el archivo exite en el directorio
+file.exists("getdata_projectfiles_UCI HAR Dataset.zip")
+
+#Declaro el zip como filename y lo descomprimo
+filename <- "getdata_projectfiles_UCI HAR Dataset.zip"
+unzip(filename)
+
+#Asigno nombres a todas las bases de datos
+features <- read.table("UCI HAR Dataset/features.txt", col.names = c("n","functions"))
+activities <- read.table("UCI HAR Dataset/activity_labels.txt", col.names = c("code", "activity"))
+subject_test <- read.table("UCI HAR Dataset/test/subject_test.txt", col.names = "subject")
+x_test <- read.table("UCI HAR Dataset/test/X_test.txt", col.names = features$functions)
+y_test <- read.table("UCI HAR Dataset/test/y_test.txt", col.names = "code")
+subject_train <- read.table("UCI HAR Dataset/train/subject_train.txt", col.names = "subject")
+x_train <- read.table("UCI HAR Dataset/train/X_train.txt", col.names = features$functions)
+y_train <- read.table("UCI HAR Dataset/train/y_train.txt", col.names = "code")
+
+#Fusionar training and test
+X <- rbind(x_train, x_test)
+Y <- rbind(y_train, y_test)
+Subject <- rbind(subject_train, subject_test)
+Merged_Data <- cbind(Subject, Y, X)
+
+#Extraigo solo media y desviación estandar 
+TidyData <- Merged_Data %>% select(subject, code, contains("mean"), contains("std"))
+TidyData$code <- activities[TidyData$code, 2]
+
+#Nombrar apropiadamente las variables
+names(TidyData)[2] = "activity"
+names(TidyData)<-gsub("Acc", "Accelerometer", names(TidyData))
+names(TidyData)<-gsub("Gyro", "Gyroscope", names(TidyData))
+names(TidyData)<-gsub("BodyBody", "Body", names(TidyData))
+names(TidyData)<-gsub("Mag", "Magnitude", names(TidyData))
+names(TidyData)<-gsub("^t", "Time", names(TidyData))
+names(TidyData)<-gsub("^f", "Frequency", names(TidyData))
+names(TidyData)<-gsub("tBody", "TimeBody", names(TidyData))
+names(TidyData)<-gsub("-mean()", "Mean", names(TidyData), ignore.case = TRUE)
+names(TidyData)<-gsub("-std()", "STD", names(TidyData), ignore.case = TRUE)
+names(TidyData)<-gsub("-freq()", "Frequency", names(TidyData), ignore.case = TRUE)
+names(TidyData)<-gsub("angle", "Angle", names(TidyData))
+names(TidyData)<-gsub("gravity", "Gravity", names(TidyData))
+
+#Crear la base de datos final
+FinalData <- TidyData %>%
+  group_by(subject, activity) %>%
+  summarise_all(funs(mean))
+write.table(FinalData, "FinalData.txt", row.name=FALSE)
+
+#Resultado final
+str(FinalData)
+FinalData
